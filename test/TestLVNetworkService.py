79c76ace7dfd63bd2c8a2d69905c5c8b143d49fa
leovsch <leo.25.1996@gmail.com>
2026-05-12 14:30:58 +0200

    add test with mv network

--- test/TestLVNetworkService.py
+++ test/TestLVNetworkService.py
@@ -56,35 +56,39 @@ class Test(unittest.TestCase):
     def test_power_flow_input_power_equals_output_power(self):
         pathlist = Path("").glob('**/*.esdl')
         for path in pathlist:
-            path_in_str = str(path) 
+            path_in_str = str(path)
             with self.subTest(f"Test esdl file {path_in_str}"):
                 # Arrange
                 service, energy_system = self.int_service_and_get_energy_system(path_in_str)
-
+    
                 params = {}
                 econnections = [asset for asset in energy_system.eAllContents() if isinstance(asset, EConnection)]
                 for econnection in econnections:
                     params[f"EConnection/aggregated_active_power/{econnection.id}"] = [1000, 1000, 1000]
                     params[f"EConnection/aggregated_reactive_power/{econnection.id}"] = [0, 0, 0]
-
+    
                 # Execute
                 ret_val = service.load_flow_current_step(params, datetime(2024, 1, 1), TimeStepInformation(1, 2), "test-id",
                                                          energy_system)
-
+    
                 # Assert
                 written_datapoints = service.influx_connector.data_points
                 for data_point in written_datapoints:
                     if "home" in data_point.output_name and ".4" not in data_point.output_name:
                         self.assertNotEqual(data_point.value, 0)
-    
+        
                 total_active_power_params = sum([sum(params[key]) * 1e-3 for key in params.keys() if "aggregated_active_power" in key])
                 total_reactive_power_params = sum([sum(params[key]) * 1e-3 for key in params.keys() if "aggregated_reactive_power" in key])
                 
-                total_active_power, total_reactive_power, active_power_losses, reactive_power_losses = self.get_total_active_and_reactive_power(service, "Transformer.Transformer1")
-
+                total_active_power, total_reactive_power, active_power_losses, reactive_power_losses = self.get_total_active_and_reactive_power(service, "Transformer.highvoltagetrafo")
+    
                 self.assertAlmostEqual(total_active_power_params + active_power_losses, total_active_power, delta=1e-1)
                 self.assertAlmostEqual(total_reactive_power_params + reactive_power_losses, total_reactive_power, delta=1e-1)
 
+
+    mv_file_names = [
+        "mv-network-test.esdl"
+    ]
 
     def test_init_builds_dss_file_correctly(self):
         pathlist = Path("").glob('**/*.esdl')
@@ -95,7 +99,7 @@ class Test(unittest.TestCase):
                 dss_content = []
                 with open(service.dss_file_name, "r") as f:
                     dss_content = f.readlines()
-
+    
                 lines_section_start = dss_content.index(service.lines_section_start_marker) + 1
                 amount_of_lines_dss = dss_content[lines_section_start:].index("\n")
                 trafo_section_start = dss_content.index(service.transformer_section_start_marker) + 1
@@ -104,9 +108,13 @@ class Test(unittest.TestCase):
                 amount_of_loads_dss = dss_content[load_section_start:].index("\n")
 
                 amount_of_electricity_cables = len([element for element in energy_system.eAllContents() if isinstance(element, ElectricityCable)])
+
+                if path_in_str in self.mv_file_names:
+                    amount_of_electricity_cables -= 1
+
                 amount_of_transformers = len([element for element in energy_system.eAllContents() if isinstance(element, Transformer)])
                 amount_of_econnections = len([element for element in energy_system.eAllContents() if isinstance(element, EConnection)])
-
+    
                 self.assertEqual(amount_of_lines_dss, amount_of_electricity_cables)
                 self.assertEqual(amount_of_transformers, amount_of_transformers_dss)
                 self.assertEqual(amount_of_econnections * 3, amount_of_loads_dss)
